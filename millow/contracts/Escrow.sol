426b5080cd49d974fa39e17f229791447efc925c
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-01-26 23:18:39 +0530

    contract finished

--- millow/contracts/Escrow.sol
+++ millow/contracts/Escrow.sol
@@ -27,10 +27,18 @@ contract Escrow {
     _;
    }
 
+
+   modifier onlyInspector() {
+      require(msg.sender == inspector, "Only inspector can call this method");
+      _;
+     }
+
    mapping(uint256 => bool) public isListed;
    mapping(uint256 => uint256) public purchasePrice;
    mapping(uint256 => uint256) public  escrowAmount;
    mapping(uint256 => address) public  buyer;
+   mapping(uint256 => bool) public  inspectionPassed;
+   mapping(uint256 => mapping(address => bool)) public approval;
 
    constructor(address _nftAddress , address payable _seller , address _inspector , address _lender) {
       nftAddress = _nftAddress;
@@ -60,20 +68,50 @@ contract Escrow {
    }
 
 
+   function updateInspectionStatus(uint256 _nftID , bool _passed) public onlyInspector {
+      inspectionPassed[_nftID] = _passed;
+   }
+
+
+   function approveSale(uint256 _nftID) public {
+      approval[_nftID][msg.sender] = true;
+   }
+
+   // finishing contract 
+
+
+   function finalizeSale(uint256 _nftID) public {
+      require(inspectionPassed[_nftID]);
+      require(approval[_nftID][buyer[_nftID]]);
+      require(approval[_nftID][seller]);
+      require(approval[_nftID][lender]);
+      require(address(this).balance >= purchasePrice[_nftID]);
+
+      isListed[_nftID] = false;
+
+
+      (bool success , ) =  payable(seller).call{value: address(this).balance}("");
+      require(success);
+
+
+      IERC721(nftAddress).transferFrom(address(this), buyer[_nftID] , _nftID);
+   }
+
+
+   function cancelSale(uint256 _nftID) public {
+      if(inspectionPassed[_nftID] == false) {
+         payable(buyer[_nftID]).transfer(address(this).balance);
+      } else {
+         payable(seller).transfer(address(this).balance);
+      }
+   }
+
    receive() external payable { }
 
 
    function getBalance() public view returns (uint256) {
     return address(this).balance;
    }
-
-   // finishing contract 
-
-
-
-   
-
-
 
 
 }
