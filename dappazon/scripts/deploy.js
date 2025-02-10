c9d49a981f6ecb860992238a07c7743df31c49d6
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-02-10 21:16:44 +0530

    deploy script

--- dappazon/scripts/deploy.js
+++ dappazon/scripts/deploy.js
@@ -12,6 +12,33 @@ const tokens = (n) => {
 }
 
 async function main() {
+  const [deployer] = await ethers.getSigners()
+
+  const Dappazon = await hre.ethers.getContractFactory("Dappazon")
+  const dappazon = await Dappazon.deploy()
+  await dappazon.deployed()
+
+  // listing items
+
+  console.log(`Deployed Dappazon Contract at: ${dappazon.address}\n`)
+
+
+  for (let i = 0; i < items.length; i++) {
+    const transaction = await dappazon.connect(deployer).list(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock,
+    )
+
+    await transaction.wait()
+
+    console.log(`Listed item ${items[i].id}: ${items[i].name}`)
+  }
+
 
 }
 
