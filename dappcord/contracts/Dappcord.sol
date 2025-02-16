b766a8562620321a60459dfc597ce8eaa6311f4c
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-02-16 22:33:22 +0530

    creating channels

--- dappcord/contracts/Dappcord.sol
+++ dappcord/contracts/Dappcord.sol
@@ -3,11 +3,36 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Dappcord is ERC721{
+contract Dappcord is ERC721 {
+    uint256 public totalChannels;
     address public owner;
 
+    struct Channel {
+        uint256 id;
+        string name;
+        uint256 cost;
+    }
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name , _symbol){
+    mapping(uint256 => Channel) public channels;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {
         owner = msg.sender;
+    }
+
+    function createChannel(string memory _name, uint _cost) public onlyOwner {
+        totalChannels++;
+        channels[totalChannels] = Channel(1, _name, _cost);
+    }
+
+    function getChannel(uint256 _id) public view returns (Channel memory) {
+        return channels[_id];
     }
 }
--- dappcord/test/Dappcord.js
+++ dappcord/test/Dappcord.js
@@ -15,6 +15,10 @@ describe("Dappcord", function () {
     [deployer, user] = await ethers.getSigners()
     const Dappcord = await ethers.getContractFactory("Dappcord")
     dappcord = await Dappcord.deploy(NAME, SYMBOL)
+
+    // create a channel 
+    const transition = await dappcord.connect(deployer).createChannel("general", tokens(1))
+    await transition.wait()
   })
 
   describe("Deployment", function () {
@@ -33,6 +37,22 @@ describe("Dappcord", function () {
     it("Sets the owner", async () => {
       let result = await dappcord.owner()
       expect(result).to.equal(deployer.address)
+    })
+  })
+
+
+  describe("Creating Channels", function () {
+    it("Returns total channels", async () => {
+      const result = await dappcord.totalChannels()
+      expect(result).to.be.equal(1)
+    })
+
+
+    it("Returns channel attributes", async () => {
+      const channel = await dappcord.getChannel(1)
+      expect(channel.id).to.be.equal(1)
+      expect(channel.name).to.be.equal("general")
+      expect(channel.cost).to.be.equal(tokens(1))
     })
   })
 })
