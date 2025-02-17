139553553fff44f27fb1bf73a15ce959537be320
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-02-17 22:58:10 +0530

    deployment setup

--- dappcord/scripts/deploy.js
+++ dappcord/scripts/deploy.js
@@ -6,6 +6,23 @@ const tokens = (n) => {
 
 async function main() {
 
+  const [deployer] = await ethers.getSigners();
+  const NAME = "Dappcord"
+  const SYMBOL = "DC"
+
+  // deploy cntracts 
+  const Dappcord = await ethers.getContractFactory("Dappcord")
+  const dappcord = await Dappcord.deploy(NAME, SYMBOL)
+  await dappcord.deployed()
+
+  // Create 3 Channels 
+  const CHANNEL_NAMES = ["general", "intro", "jobs"]
+  const COSTS = [tokens(1), tokens(0), tokens(0.25)]
+
+  for (let i = 0; i < 3; i++) {
+    const transcation = await dappcord.connect(deployer).createChannel(CHANNEL_NAMES[i], COSTS[i])
+    await transcation.wait()
+  }
 }
 
 main().catch((error) => {
