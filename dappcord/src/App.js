27be217a81685493e01cf837c5352f85c5d92f7f
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-02-17 23:25:10 +0530

    frontend connection and setup

--- dappcord/src/App.js
+++ dappcord/src/App.js
@@ -19,10 +19,21 @@ const socket = io('ws://localhost:3030');
 
 function App() {
 
+  const [account, setAccount] = useState(null);
+
+  const loadBlockchainData = async () => {
+    window.ethereum.on('accountsChanged', async () => {
+      window.location.reload()
+    })
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  })
+
   return (
     <div>
-      <h1 style={{ textAlign: "center", padding: "15px" }}>Welcome to Dappcord</h1>
-
+      <Navigation account={account} setAccount={setAccount} />
       <main>
 
       </main>
