65bbed1cf260d171581a8dc8498ea7753d3c0167
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-02-22 23:52:56 +0530

    token setup

--- tokenmaster/contracts/TokenMaster.sol
+++ tokenmaster/contracts/TokenMaster.sol
@@ -1,4 +1,10 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract TokenMaster {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TokenMaster is ERC721 {
+    constructor(string memory _name, string memory _symbol) ERC721(_name , _symbol) {
+        
+    }
+}
