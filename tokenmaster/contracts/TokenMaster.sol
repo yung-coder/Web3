7a88ec834c9068443eae3cde4fb273aa21a76ad8
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-02-23 19:42:44 +0530

    test setup

--- tokenmaster/contracts/TokenMaster.sol
+++ tokenmaster/contracts/TokenMaster.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TokenMaster is ERC721 {
-    constructor(string memory _name, string memory _symbol) ERC721(_name , _symbol) {
-        
-    }
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {}
 }
--- tokenmaster/test/TokenMaster.js
+++ tokenmaster/test/TokenMaster.js
@@ -1,5 +1,26 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+
+const NAME = "TokenMaster";
+const SYMBOL = "TM";
 
 describe("TokenMaster", () => {
+  let tokenMaster;
+  let deployer, buyer;
 
-})
+  beforeEach(async () => {
+    [deployer, buyer] = await ethers.getSigners();
+    const TokenMaster = await ethers.getContractFactory("TokenMaster");
+    tokenMaster = await TokenMaster.deploy(NAME, SYMBOL);
+  });
+  describe("Deployment", () => {
+    it("Sets the name", async () => {
+      let name = await tokenMaster.name();
+      expect(name).to.equal("TokenMaster");
+    });
+
+    it("Sets the synmbol", async () => {
+      let symbol = await tokenMaster.symbol();
+      expect(symbol).to.equal("TM");
+    });
+  });
+});
