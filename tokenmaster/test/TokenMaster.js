038b4a4bca8b5d64389d092a4644b5d6b2ef5e25
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-02-23 20:37:38 +0530

    add events

--- tokenmaster/test/TokenMaster.js
+++ tokenmaster/test/TokenMaster.js
@@ -3,6 +3,13 @@ const { expect } = require("chai");
 const NAME = "TokenMaster";
 const SYMBOL = "TM";
 
+const OCCASION_NAME = "ETH Texas";
+const OCCASION_COST = ethers.utils.parseUnits("1", "ether");
+const OCCASION_MAX_TICKETS = 100;
+const OCCASION_DATE = "Apr 27";
+const OCCASION_TIME = "10:00AM CST";
+const OCCASION_LOCATION = "Austin, Texas";
+
 describe("TokenMaster", () => {
   let tokenMaster;
   let deployer, buyer;
@@ -11,6 +18,19 @@ describe("TokenMaster", () => {
     [deployer, buyer] = await ethers.getSigners();
     const TokenMaster = await ethers.getContractFactory("TokenMaster");
     tokenMaster = await TokenMaster.deploy(NAME, SYMBOL);
+
+    let transcation = await tokenMaster
+      .connect(deployer)
+      .list(
+        OCCASION_NAME,
+        OCCASION_COST,
+        OCCASION_MAX_TICKETS,
+        OCCASION_DATE,
+        OCCASION_TIME,
+        OCCASION_LOCATION
+      );
+
+    await transcation.wait();
   });
   describe("Deployment", () => {
     it("Sets the name", async () => {
@@ -21,6 +41,28 @@ describe("TokenMaster", () => {
     it("Sets the synmbol", async () => {
       let symbol = await tokenMaster.symbol();
       expect(symbol).to.equal("TM");
+    });
+
+    it("Sets the owner", async () => {
+      expect(await tokenMaster.owner()).to.equal(deployer.address);
+    });
+  });
+
+  describe("Occasions", () => {
+    it("Update occasions count", async () => {
+      const totalOccasions = await tokenMaster.totalOccasions();
+      expect(totalOccasions).to.be.equal(1);
+    });
+
+    it("Returns occasions attributes", async () => {
+      const occasion = await tokenMaster.getOccasion(1);
+      expect(occasion.id).to.be.equal(1);
+      expect(occasion.name).to.be.equal(OCCASION_NAME);
+      expect(occasion.cost).to.be.equal(OCCASION_COST);
+      expect(occasion.tickets).to.be.equal(OCCASION_MAX_TICKETS);
+      expect(occasion.date).to.be.equal(OCCASION_DATE);
+      expect(occasion.time).to.be.equal(OCCASION_TIME);
+      expect(occasion.location).to.be.equal(OCCASION_LOCATION);
     });
   });
 });
